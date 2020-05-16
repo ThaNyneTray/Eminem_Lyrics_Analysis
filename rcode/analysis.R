83c9c2a8f9dd3f8191b0b756e9c5f71dcea5def2
Desmond Tuiyot <dtuiyot@gmail.com>
2020-05-16 19:33:58 -0400

    Analysis on Word frequencies across albums, and function to plot to visualize word frequency similarities between pairs of albums

--- rcode/analysis.R
+++ rcode/analysis.R
@@ -80,6 +80,129 @@ lyrics
 ############## WORD FREQUENCIES ##################################################
 ##################################################################################
 
+
+# 1. here we use unnest tokens to get a column of words (tidy text) on its own, which 
+#    streamlines the analysis process
+# 2. we also remove stop words, using anti join
+
+data("stop_words")
+
+tidy_lyrics<-lyrics %>%
+  unnest_tokens(word, lyric) %>%
+  anti_join(stop_words) 
+
+words_total<-tidy_lyrics %>%
+  count(word) %>%
+  arrange(-n)
+
+words_total
+# 3. In true Eminem fashion, we find some curses amongst his top 10 most used
+# 4. Also notice that no 2 and 4 are practically the same. THis would be the 
+#    perfect time to add some lemmatization
+
+# words_ld<-tidy_lyrics %>%
+#   mutate(lemma=textstem::lemmatize_words(word)) %>%
+#   select(word, lemma) %>%
+#   filter(word != lemma) %>%
+#   distinct(word, lemma)
+
+# 5. let;s see what difference it makes in total count
+words_total_lemma<-tidy_lyrics %>%
+  mutate(lemma=textstem::lemmatize_words(word)) %>%
+  # group_by(lemma) %>%
+  count(lemma) %>%
+  arrange(-n)
+
+# 6. THe lemmatization caught  some extra variations of some words,
+#    However, it missed "fucking" because of the missing 'g' in its gerund
+# 7. We can modify this manually. At the same time, yeah was corrected to yeah,
+#    I don't like that. So we correct it.
+# 8. First, store the unlemmatized lyrics, in case we need them later
+lyrics_unlemma<-tidy_lyrics
+
+tidy_lyrics<-tidy_lyrics %>%
+  mutate(word=textstem::lemmatize_words(word),
+         word=ifelse(word=="yes", "yeah", word),
+         word=ifelse(word=="fuckin", "fuck", word))
+
+# 9. Finally, we get the frequency, and split by album
+# 10.TOtal frequency
+tidy_lyrics %>%
+  count(word) %>%
+  arrange(-n)
+
+# 11. Let's see the frequency difference across albums
+# 12. It might be more reasonable to look at the proportions as
+#     opposed to raw counts. The album lengths are bound to be different
+# 13. Basically, which words appear in highest proportion in each album?
+freq<-tidy_lyrics %>%
+  count(album, word) %>%
+  # arrange(-n) %>%
+  group_by(album) %>%
+  mutate(total=sum(n)) %>%
+  # ungroup() %>%
+  mutate(freq=n/total)
+
+freq
+# 14. To make it easier to see, let's plot it
+#     TODO: come back to this. How to rearrange plot
+freq %>%
+  group_by(album) %>%
+  # top_n(10, freq) %>%
+  arrange(-freq) %>%
+  mutate(word=factor(word, freq)) %>%
+  ggplot(aes(word, freq, fill=album)) +
+  geom_col(show.legend = FALSE) +
+  # facet_wrap(~album, ncol=3, scales="free") +
+  coord_flip() + theme_bw()
+
+# 15. let's see about word correlations,
+#     We wanna spread the values of the album row to take individual columns.
+freq<-freq %>%
+  select(album, word, freq) %>%
+  spread(album, freq, fill=0)
+
+# 16. Here since we are using a bivariate plot, let's choose some albums to compare
+#     WIth how terriby Revival was received, one of the mottos was 
+#     "Bring the Real Slim Shady back". So let's see how it compares to 
+#     Slim Shady LP
+sslp_revival<-freq %>%
+  select(word, `The Slim Shady LP`, Revival) %>%
+  arrange(-Revival, -`The Slim Shady LP`) %>%
+  filter(!Revival==0 & !`The Slim Shady LP`==0)
+
+ggplot(sslp_revival, aes(`The Slim Shady LP`, Revival)) +
+  geom_jitter(alpha=0.1, size=2.5, width=0.25, height=0.25)+
+  geom_text(aes(label=word), check_overlap=TRUE, vjust=1.5) +
+  scale_x_log10(labels=scales::percent_format())+
+  scale_y_log10(labels=scales::percent_format())+
+  geom_abline(color="red") +
+  theme_bw()
+
+# 17. Since this is something we might have to do repeatedly for different pairs of 
+#     albums, let's wrap it in a function
+albums_corplot<-function(album1, album2, freq){
+  album1<-enquo(album1)
+  album2<-enquo(album2)
+  
+  freq<-freq %>%
+    select(word, !!album1, !!album2) %>%
+    filter(!(!!album1==0) & !(!!album2==0))
+  print(album1)
+  print(album2)
+  ggplot(freq, aes(!!album1, !!album2)) +
+    geom_jitter(alpha=0.1, size=2.5, width=0.25, height=0.25)+
+    geom_text(aes(label=word), check_overlap=TRUE, vjust=1.5) +
+    scale_x_log10(labels=scales::percent_format())+
+    scale_y_log10(labels=scales::percent_format())+
+    geom_abline(color="red") +
+    theme_bw()
+}
+
+albums_corplot(`The Slim Shady LP`, Revival, freq)
+albums_corplot(`The Marshall Mathers LP`, `The Marshall Mathers LP2`, freq)
+albums_corplot(Kamikaze, Revival, freq)
+
 ##################################################################################
 ############## COMPARING WORD USAGE ##############################################
 ##################################################################################
